f831a0621b9d99fd3370a80122ae6a8bd0826299
armillaire <armillaire@tutanota.com>
2024-09-26 20:19:35 -0500

    ahh

--- main.cc
+++ main.cc
@@ -8,6 +8,7 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+/* TODO change this to be not global (if possible) */
 struct EditorConfig {
     int screen_rows, screen_cols;
     struct termios original_termios;
@@ -22,6 +23,7 @@ void editor_process_keypress();
 char editor_read_key();
 void editor_refresh_screen();
 void enable_raw_mode();
+int get_cursor_position(int *rows, int *cols);
 int get_window_size(int *rows, int *cols);
 void init_editor();
 
@@ -128,11 +130,43 @@ enable_raw_mode() {
 }
 
 int
+get_cursor_position(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+
+        if (buf[i] == 'R')
+            break;
+
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+
+    return 0;
+}
+
+int
 get_window_size(int *rows, int *cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+
+        return get_cursor_position(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
