de34e874f1ce871536989b81f105697e1810dad3
armillaire <armillaire@tutanota.com>
2024-09-28 10:39:08 -0500

    ahhh funny movement

--- main.cc
+++ main.cc
@@ -7,10 +7,21 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+#define VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+enum EditorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+};
 
 /* TODO change this to be not global (if possible) */
 struct EditorConfig {
+    int c_x, c_y;
     int screen_rows, screen_cols;
     struct termios original_termios;
 };
@@ -26,7 +37,8 @@ struct a_buf {
         this->free();
     }
 
-    void append(const char *s, int len) {
+    void
+    append(const char *s, int len) {
         /* TODO find which c++ converter goes here */
         char *n_raw = (char *)realloc(this->raw, this->len + len);
 
@@ -38,7 +50,8 @@ struct a_buf {
         this->len += len;
     }
 
-    void free() {
+    void
+    free() {
         ::free(this->raw);
     }
 };
@@ -47,9 +60,10 @@ struct EditorConfig E;
 
 void die(const char *s);
 void disable_raw_mode();
-void editor_draw_fringe();
+void editor_draw_rows(struct a_buf *ab);
+void editor_move_cursor(int key);
 void editor_process_keypress();
-char editor_read_key();
+int editor_read_key();
 void editor_refresh_screen();
 void enable_raw_mode();
 int get_cursor_position(int *rows, int *cols);
@@ -85,32 +99,81 @@ disable_raw_mode() {
         die("tcsetattr");
 }
 
-/* prints the fringe of ~ on the edges */
-void editor_draw_fringe() {
+void editor_draw_rows(struct a_buf *ab) {
     for (int i = 0; i < E.screen_rows; i++) {
-        write(STDOUT_FILENO, "~", 1);
+        if (i == E.screen_rows / 3) {
+            char welcome[80];
+            char welcome_len = snprintf(welcome, sizeof(welcome),
+                    "uiua ide -- version %s", VERSION);
+            
+            if (welcome_len > E.screen_cols)
+                welcome_len = E.screen_cols;
 
+            int padding = (E.screen_cols - welcome_len) / 2;
+            if (padding) {
+                ab->append("~", 1);
+                padding--;
+            }
+
+            while (padding--)
+                ab->append(" ", 1);
+
+            ab->append(welcome, welcome_len);
+        } else {
+            ab->append("~", 1);
+        }
+
+        ab->append("\x1b[K", 3);
         if (i < E.screen_rows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            ab->append("\r\n", 2);
         }
     }
 }
 
 void
-editor_process_keypress() {
-    char c = editor_read_key();
-
-    switch (c) {
-        case CTRL_KEY('q'):
-            write(STDOUT_FILENO, "\x1b[2J", 4);
-            write(STDOUT_FILENO, "\x1b[H", 3);
-            
-            exit(0);
-            break;
+editor_move_cursor(int key) {
+    switch (key) {
+    case ARROW_LEFT:
+        if (E.c_x != 0)
+            E.c_x--;
+        break;
+    case ARROW_RIGHT:
+        if (E.c_x != E.screen_cols - 1)
+            E.c_x++;
+        break;
+    case ARROW_UP:
+        if (E.c_y != 0)
+            E.c_y--;
+        break;
+    case ARROW_DOWN:
+        if (E.c_y != E.screen_rows - 1)
+            E.c_y++;
+        break;
     }
 }
 
-char
+void
+editor_process_keypress() {
+    int c = editor_read_key();
+
+    switch (c) {
+    case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+        
+        exit(0);
+        break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editor_move_cursor(c);
+        break;
+    }
+}
+
+int
 editor_read_key() {
     int nread;
     char c;
@@ -119,17 +182,57 @@ editor_read_key() {
             die("read");
     }
 
-    return c;
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                    case '3': return DEL_KEY;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+                }
+            }
+        }
+
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 void
 editor_refresh_screen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct a_buf ab;
 
-    editor_draw_fringe();
+    ab.append("\x1b[?25l", 6);
+    ab.append("\x1b[H", 3);
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editor_draw_rows(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.c_y + 1, E.c_x + 1);
+    ab.append(buf, strlen(buf));
+
+    ab.append("\x1b[?25h", 6);
+    
+    write(STDOUT_FILENO, ab.raw, ab.len);
 }
 
 void
@@ -209,6 +312,9 @@ get_window_size(int *rows, int *cols) {
 
 void
 init_editor() {
+    E.c_x = 0;
+    E.c_y = 0;
+
     if (get_window_size(&E.screen_rows, &E.screen_cols) == -1)
         die("get_window_size");
 }
